e2d1391551fc107a17d89146dca56ffb2298ca4d
Dmitry Fomin <dmitry@everstay.net>
2025-12-07 18:44:10 -0800

    add tailwind to mason config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -496,6 +496,7 @@ require('lazy').setup({
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
         'eslint',
+        'tailwindcss',
       })
 
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
